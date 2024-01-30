df00625285e91cc5fe8b846d363932d51bee89b0
Administrator <Administrator@172.31.30.102>
2024-01-30 12:56:41 +0000

    second commit for handson

---
+++ EclipseSeleniumProject1/src/test/java/firstDemoTest/secondScript.java
@@ -0,0 +1,13 @@
+package firstDemoTest;
+
+import org.junit.Test;
+
+public class secondScript 
+{
+	@Test
+	 public void valid_UserCredential()
+	{
+
+		 System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());
+	}
+}
