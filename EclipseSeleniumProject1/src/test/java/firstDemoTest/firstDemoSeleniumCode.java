a6eceb9ac59a39e3152047f2e2e0f8e0933c6678
Administrator <Administrator@172.31.30.102>
2024-01-30 12:11:03 +0000

    first commit done on 30/1/24

---
+++ EclipseSeleniumProject1/src/test/java/firstDemoTest/firstDemoSeleniumCode.java
@@ -0,0 +1,44 @@
+package firstDemoTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class firstDemoSeleniumCode 
+{
+	private static WebDriver driver;
+ 	
+
+ @BeforeClass
+ public static void openBrowser()
+ {
+	 WebDriverManager.chromedriver().setup();
+     driver = new ChromeDriver();
+     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	} 
+
+ @Test
+ public void valid_UserCredential(){
+
+	 System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());
+     driver.get("https://demoqa.com/text-box");	
+//     driver.findElement(By.xpath(".//*[@id='account']/a")).click();
+//     driver.findElement(By.id("log")).sendKeys("testuser_3");
+//     driver.findElement(By.id("pwd")).sendKeys("Test@123");
+//     driver.findElement(By.id("login")).click();
+   
+     System.out.println("Ending test " + new Object(){}.getClass().getEnclosingMethod().getName());
+ }
+
+ @AfterClass
+ public static void closeBrowser(){
+	 driver.quit();
+ }
+}
