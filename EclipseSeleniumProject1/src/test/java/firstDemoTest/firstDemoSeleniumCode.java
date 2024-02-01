53dc2b3f0d3ca8b365d19ecc8dd196b93d421f7a
Administrator <Administrator@172.31.3.162>
2024-02-01 06:05:56 +0000

    comitt on 1/2/24 by gaurav for window handling

--- EclipseSeleniumProject1/src/test/java/firstDemoTest/firstDemoSeleniumCode.java
+++ EclipseSeleniumProject1/src/test/java/firstDemoTest/firstDemoSeleniumCode.java
@@ -1,13 +1,16 @@
 package firstDemoTest;
 
 import java.time.Duration;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -26,21 +29,44 @@ public class firstDemoSeleniumCode
 	} 
 
  @Test
- public void valid_UserCredential()
+ public void valid_UserCredential() throws InterruptedException
  {
 
 	 System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());
-     driver.get("https://demoqa.com/text-box");	
-//     driver.findElement(By.xpath(".//*[@id='account']/a")).click();
-//     driver.findElement(By.id("log")).sendKeys("testuser_3");
-//     driver.findElement(By.id("pwd")).sendKeys("Test@123");
-//     driver.findElement(By.id("login")).click();
-   
+     driver.get("https://demoqa.com/automation-practice-form");
+     driver.manage().window().maximize();
+     Thread.sleep(2000);
+     WebElement scrollDown=driver.findElement(By.xpath("//*[@id=\"app\"]//div[@class='accordion']/div[3]/span"));
+     JavascriptExecutor js=(JavascriptExecutor) driver;
+     js.executeScript("arguments[0].scrollIntoView();", scrollDown);
+     Thread.sleep(2000);
+     driver.findElement(By.xpath("//*[@id=\"app\"]//div[@class='accordion']/div[3]/span")).click();
+     Thread.sleep(2000);
+     driver.findElement(By.xpath("//*[@id=\"app\"]//div[@class='accordion']/div[3]//ul//li[1]")).click();
+     Thread.sleep(2000);
+     driver.findElement(By.xpath("//*[@id='windowButton']")).click();
+     String pWindow=driver.getWindowHandle();
+     System.out.println("Parent window id is "+pWindow);
+   Set<String>windows=driver.getWindowHandles();
+   for(String window:windows)
+   {
+	   if(!window.equals(pWindow))
+	   {
+		   driver.switchTo().window(window);
+		   driver.manage().window().maximize();
+		   String title=driver.getTitle();
+		   System.out.println("title of new window tab is "+title.toString());
+		   Thread.sleep(1000);
+		   String text=driver.findElement(By.xpath("//*[@id='sampleHeading']")).getText();
+		   System.out.println("Present Text on new window tab is--"+text);
+	   }
+	   
+   }
      System.out.println("Ending test " + new Object(){}.getClass().getEnclosingMethod().getName());
  }
 
  @AfterClass
  public static void closeBrowser(){
-	 driver.quit();
+	 //driver.quit();
  }
 }
